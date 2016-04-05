d43801312608721fb5fa9c8345b8df9759f6262f
sohamchatterjee50 <sohamchatterjee50@gmail.com>
2016-04-05 20:29:51 +0530

    WEATHER APP WITH  MAP LOCATION, SHARING FEATURES

--- app/src/main/java/com/example/sohamchatterjee/weather_21/MainActivity.java
+++ app/src/main/java/com/example/sohamchatterjee/weather_21/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.sohamchatterjee.weather_21;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,13 +14,19 @@ import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
+import android.content.SharedPreferences;
+import android.net.Uri;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import android.preference.PreferenceManager;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -57,11 +64,53 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+
+            startActivity( new Intent(this,SettingsActivity.class));
             return true;
         }
 
+        if (id == R.id.action_map) {
+                       openPreferredLocationInMap();
+                        return true;
+                    }
+
         return super.onOptionsItemSelected(item);
     }
+
+
+    private void openPreferredLocationInMap() {
+                SharedPreferences sharedPrefs =
+                        PreferenceManager.getDefaultSharedPreferences(this);
+               String location = sharedPrefs.getString(
+                         getString(R.string.pref_location_key),
+                         getString(R.string.pref_location_default));
+
+                // Using the URI scheme for showing a location found on a map.  This super-handy
+                         // intent can is detailed in the "Common Intents" page of Android's developer site:
+                                // http://developer.android.com/guide/components/intents-common.html#Maps
+                                               Uri geoLocation = Uri.parse("geo:0,0?").buildUpon()
+                                .appendQueryParameter("q", location)
+                                .build();
+
+                        Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(geoLocation);
+
+                        if (intent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(intent);
+                    } else {
+                        Log.d(LOG_TAG, "Couldn't call " + location + ", no receiving apps installed!");
+                    }
+            }
+
+
+
+
+
+
+
+
+
+
 
 
 }
